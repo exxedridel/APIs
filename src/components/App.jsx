9186536bc5b632d2f8c2f36b86be2e568c5b906f
hevedrios <exxed.ridel@gmail.com>
2022-08-15 19:22:09 -0500

    Dog API Fetch and DOM Practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,20 @@ import React from "react";
 import "../styles/global.scss";
 
 const App = () => {
+
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("image-container").innerHTML = `
+            <img class="dog-img" src="${data.message}" />
+        `;
+    });
+
   return (
     <div>
-      <h1>This is gonna be an App</h1>
+      <h1 className="title">Dog API Fetch and DOM Practice</h1>
+      <div id="image-container"></div>
     </div>
   );
 };

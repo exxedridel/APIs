f1fccc027a264818a6b855c90fcc65a10c70270e
hevedrios <exxed.ridel@gmail.com>
2022-08-19 21:08:40 -0500

    BlogSpace - New post form

--- src/components/BlogPosts.jsx
+++ src/components/BlogPosts.jsx
@@ -2,25 +2,37 @@ import React from "react";
 import "../styles/BlogPosts.scss";
 
 const BlogPosts = () => {
-  
-  fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-    .then(res => res.json())
-    .then(data => {
-        const postsArr = data.slice(0, 5)
-        let html = ""
-        for (let post of postsArr) {
-            html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `
-        }
-        document.getElementById("blog-list").innerHTML = html
-    })
+  const [allPosts, setAllPosts] = React.useState([]);
+
+  React.useEffect(function () {
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+      .then((res) => res.json())
+      .then((data) => setAllPosts(data.slice(0, 5)));
+  }, []);
+
+  const postsElements = allPosts.map((post) => {
+    return (
+      <>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+        <hr />
+      </>
+    );
+  });
 
   return (
     <div>
-      <div id="blog-list"></div>
+      <nav className="blog-navbar">
+        <h3>BlogSpace</h3>
+      </nav>
+      <form className="new-post">
+        <label htmlFor="new-title">Title: </label>
+        <input id="new-title" type="text" />
+        <label htmlFor="new-body">Body: </label>
+        <textarea id="new-body" />
+        <button>Post</button>
+      </form>
+      <div id="blog-list">{postsElements}</div>
     </div>
   );
 };

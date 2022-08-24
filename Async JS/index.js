b67e8a90ce06904dbb704320a22bfd380ae2e3f0
hevedrios <exxed.ridel@gmail.com>
2022-08-23 19:13:42 -0500

    War - Completed fetch and Styling

--- Async JS/index.js
+++ Async JS/index.js
@@ -1,19 +1,27 @@
-let deckId = ""
+let deckId;
 
-document.getElementById("btn-new-deck").addEventListener("click", () => {
+function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
     .then((data) => {
-      deckId = data.deck_id
-      console.log(deckId)
+      console.log(data);
+      deckId = data.deck_id;
+    });
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick);
+
+document.getElementById("draw-cards").addEventListener("click", () => {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.cards);
+      document.getElementById("cards").innerHTML = `
+                <img src=${data.cards[0].image} />
+                <img src=${data.cards[1].image} />
+            `;
     });
 });
-
-document.getElementById("btn-draw-cards").addEventListener("click", () => {
-   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
-       .then(res => res.json())
-       .then(data => console.log(data))
-})
 
 /* --------------------------------------------Exercises--------------------------------------------- */
 
@@ -49,37 +57,39 @@ const people = [
  * Note: Remember that your callback function will be given the individual item in the array for a parameter
  */
 
- function filterArray(array, callback) {
-   const resultingArray = []
-   // Filtering logic here
-   for (let item of array) {
-       const shouldBeIncluded = callback(item)
-       if (shouldBeIncluded) {
-           resultingArray.push(item)
-       }
-   }
-   return resultingArray
+function filterArray(array, callback) {
+  const resultingArray = [];
+  // Filtering logic here
+  for (let item of array) {
+    const shouldBeIncluded = callback(item);
+    if (shouldBeIncluded) {
+      resultingArray.push(item);
+    }
+  }
+  return resultingArray;
 }
 
-const peopleWithMajority = filterArray(people, function(person) {
-   return person.age >= 18
-})
-console.log(peopleWithMajority)
+const peopleWithMajority = filterArray(people, function (person) {
+  return person.age >= 18;
+});
+console.log(peopleWithMajority);
 
-// 3. Given the array below, chain the `.filter` and `.map` array methods together to turn the array into 
-// an array of string email addresses of only the people in the array who voted. Log the array of email 
+// 3. Given the array below, chain the `.filter` and `.map` array methods together to turn the array into
+// an array of string email addresses of only the people in the array who voted. Log the array of email
 // addresses to the console
 
 const voters = [
-   {name: "Joe", email: "joe@joe.com", voted: true},
-   {name: "Jane", email: "jane@jane.com", voted: true},
-   {name: "Bo", email: "bo@bo.com", voted: false},
-   {name: "Bane", email: "bane@bane.com", voted: false}
-]
+  { name: "Joe", email: "joe@joe.com", voted: true },
+  { name: "Jane", email: "jane@jane.com", voted: true },
+  { name: "Bo", email: "bo@bo.com", voted: false },
+  { name: "Bane", email: "bane@bane.com", voted: false },
+];
 // Write your code below
-const finalResult = voters.filter(voter => voter.voted).map(voter => voter.email)
+const finalResult = voters
+  .filter((voter) => voter.voted)
+  .map((voter) => voter.email);
 
-console.log(finalResult)
+console.log(finalResult);
 // Final result: ["joe@joe.com", "jane@jane.com"]
 
 /**
@@ -88,14 +98,14 @@ console.log(finalResult)
  * and log it to the console inside the body of this new
  * 3rd .then() block
  */
- fetch("https://apis.scrimba.com/bored/api/activity")
- .then(function(res) {
-     return res
- })
- .then(function(whatever) {
-     console.log(whatever)
-     return "World"
- })
- .then(function(another) {
-     console.log(another)
- })
+fetch("https://apis.scrimba.com/bored/api/activity")
+  .then(function (res) {
+    return res;
+  })
+  .then(function (whatever) {
+    console.log(whatever);
+    return "World";
+  })
+  .then(function (another) {
+    console.log(another);
+  });
